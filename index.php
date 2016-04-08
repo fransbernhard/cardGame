2935eba1a047a5d2a9ed6f7b628e40fd70937c63
Mikael Elmgren <mikaelelmgren@gmail.com>
2016-04-08 08:40:26 +0200

    Lite mappstruktur (classes & css) + lite exempeltext & styling för att se så allt funkar.. Trevlig helg

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+spl_autoload_register(function ($className) {
+	include 'classes/' . $className . '.class.php';
+});
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href='https://fonts.googleapis.com/css?family=Unica+One' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Vollkorn:400,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="wrapper">
+	<header><h1>The Best CardGame Ever !!</h1></header>
+	<section><p>Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Vivamus euismod mauris. Donec interdum, metus et hendrerit aliquet, dolor diam sagittis ligula, eget egestas libero turpis vel mi. Fusce ac felis sit amet ligula pharetra condimentum. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
+
+	Vestibulum dapibus nunc ac augue. Donec mollis hendrerit risus. Proin magna. Phasellus gravida semper nisi. Etiam ultricies nisi vel augue.</p></section>
+</div>
+</body>
+</html>
