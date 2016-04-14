b22292139a8ee02a7f41222e335d83ec5619fb6a
Jonathan Burns <mrburnsjonathan@gmail.com>
2016-04-14 15:13:55 +0200

    added showHand function to Player class

--- classes/Player.class.php
+++ classes/Player.class.php
@@ -21,10 +21,13 @@ class Player {
     public function layDownCard(){
         $this->hand = [1, 2, 3, 4, 5, 6];    // Hard coded numbers represent cards. Will get from deal function
         array_shift($this->hand);            // Shift the first "card" out
-    for($i=0; $i<count($this->hand); $i++){  // loops through to get the remaining "cards"
+  }
+
+    public function showHand(){
+        for($i=0; $i<count($this->hand); $i++){  // loops through to get the remaining "cards"
        echo $this->hand[$i];                 // echos out what remains in the hand array
     }
-  }
+    }
 }
 
 ?>
