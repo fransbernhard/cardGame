b9089ecbffba6636b176876542e8b4b871f1c38b
Jonathan Burns <mrburnsjonathan@gmail.com>
2016-04-14 15:01:07 +0200

    Added layDownCard function to Player class

--- classes/Player.class.php
+++ classes/Player.class.php
@@ -6,17 +6,9 @@ class Player {
 
 
     public function __construct() {             // Creates and array of players
-    $this->players = ["player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8", "player9", "player10"];
+    $this->players = ["player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8", "player9", "player10"];                    // Hard coded players in. Possbly make a create player function
 
-    //        taken out after since we do not need to loop through. Possibly totally remove.
-    // if (is_array($players) || is_object($players)){   // fixeds a failure error with the for each
-
-    //     foreach ($players as $player) {
-    //         $players[] = ["player"=>$player];
-    //     }
-    // }
 }
-
     public function showPlayer() {    // Takes a random set of payers out of a shuffled group of players
         shuffle($this->players);      // Shuffles the array of players
         $rand_players = array_rand($this->players, 4);
@@ -25,6 +17,14 @@ class Player {
         echo $this->players[$rand_players[2]] . "<br>";
         echo $this->players[$rand_players[3]] . "<br>";
     }
+
+    public function layDownCard(){
+        $this->hand = [1, 2, 3, 4, 5, 6];    // Hard coded numbers represent cards. Will get from deal function
+        array_shift($this->hand);            // Shift the first "card" out
+    for($i=0; $i<count($this->hand); $i++){  // loops through to get the remaining "cards"
+       echo $this->hand[$i];                 // echos out what remains in the hand array
+    }
+  }
 }
 
 ?>
